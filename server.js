b7f5d4e8feb8db1b2d8f9982e38bd5ce13c80ee6
Kalab Smith <kalabsb@me.com>
2025-03-28 14:31:25 -0500

    Scoreboard Finshed

--- server.js
+++ server.js
@@ -5,19 +5,21 @@ import dotenv from 'dotenv';
 
 //configure stuff
 dotenv.config();
-const connectionStringURL = process.env.MONGODBURL;
+const connectionStringURL = process.env.MONGODB_URL;
 const client = new MongoClient(connectionStringURL);
 await client.connect();
 const db = client.db('bank');
 const port = process.env.PORT || 3000;
 
 //app functions
+//add validation to make sure that only entries that make since work to sendData function
 async function sendData(req, res) {
     try {
         const room = req.body.room;
-        const data = { room: req.body.room, stage: req.body.stage, turn: req.body.turn, players: req.body.players }// helps check that structure of the data being passed in is accurate
-        const result = await db.collection('data').updateOne({ room: room }, data, { upsert: ture })
-        if (result){
+        //make it so theres data in room code
+        const data = { room: req.body.room, stage: req.body.stage, turn: req.body.turn, players: req.body.players, points: req.body.points, round: req.body.round, rounds: req.body.rounds }// helps check that structure of the data being passed in is accurate
+        const result = await db.collection('data').updateOne({ room: room }, data, { upsert: true })
+        if (result) {
             res.send(true)
         } else {
             res.send(false)
@@ -25,18 +27,28 @@ async function sendData(req, res) {
     }
     catch (err) {
         console.error(err);
+        res.status(404).json({error: err});
     }
 }
 async function getData(req, res) {
     try {
         const room = req.qarams.room;
-        const result = await db.collection('data').findOne({room: room});
+        const result = await db.collection('data').findOne({ room: room });
         if (result) {
             res.json(result)
         }
         else {
-            res.status(404).json({error: "Can't find that room code"})
+            res.status(404).json({ error: "Can't find that room code" })
         }
+    }
+    catch (err) {
+        console.error(err);
+        res.status(404).json({ error: "Can't find that room code" })
+    }
+}
+async function defaultFile(req, res) {
+    try {
+        res.sendFile((__dirname + '/public/app.html'));
     }
     catch (err) {
         console.error(err);
@@ -49,6 +61,9 @@ app.use(express.json())
 app.use(express.static('public'))
 app.post('/api/send', sendData)
 app.get('/api/get/:room', getData)
+//get route for room code setting up note also make it so it deletes old rooms too.
+//delete route for when the games finshed
+app.get('*', defaultFile)
 app.listen(port, () => { console.log(`Server started on port ${port}`) })
 
 
